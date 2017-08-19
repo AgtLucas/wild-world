7c75d6505e74c3c0331364521de3e600f0766286
AgtLucas <lucas@agtlucas.com>
2017-08-19 08:06:49 +0200

    Update page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default () => <div>Hey!</div>
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>Wild world</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
+    <p>Hello from the server</p>
+  </div>
+)
